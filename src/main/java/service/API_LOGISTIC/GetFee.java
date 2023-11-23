1d0bbcdc305cce6cb662972b2275e804f7a0909a
nguyenthanhdat20130013 <20130013@st.hcmuaf.edu.vn>
2023-11-23 11:39:26 +0700

    sửa lại phần tính phí giao hàng do api tính phí lỗi

--- src/main/java/service/API_LOGISTIC/GetFee.java
+++ src/main/java/service/API_LOGISTIC/GetFee.java
@@ -15,41 +15,41 @@ import java.io.IOException;
 
 public class GetFee {
     public static Double calculateShippingFee(String token, String from_district_id, String from_ward_id, String to_district_id, String to_ward_id) throws IOException, IOException {
-        String urlAPI = "http://140.238.54.136/api/calculateFee";
-        String height = "100";
-        String length = "100";
-        String width = "100";
-        String weight = "100";
-        // Tạo yêu cầu tính phí vận chuyển đến API
-        CloseableHttpClient client = HttpClientBuilder.create().build();
-        HttpPost post = new HttpPost("http://140.238.54.136/api/calculateFee");
-        post.setHeader("Content-type", "application/json");
-        post.setHeader("Authorization", "Bearer " + token);
-        JSONObject json = new JSONObject();
-        json.put("from_district_id", from_district_id);
-        json.put("from_ward_id", from_ward_id);
-        json.put("to_district_id", to_district_id);
-        json.put("to_ward_id", to_ward_id);
-        json.put("height", height);
-        json.put("length", length);
-        json.put("width", width);
-        json.put("weight", weight);
-        StringEntity entity = new StringEntity(json.toString());
-        post.setEntity(entity);
-
-        // Gửi yêu cầu và nhận phản hồi từ API
-        HttpResponse apiResponse = client.execute(post);
-        HttpEntity apiResponseEntity = apiResponse.getEntity();
-        String apiResponseString = EntityUtils.toString(apiResponseEntity);
-        // Trích xuất giá trị phí vận chuyển từ phản hồi API
-        String val = apiResponseString;
-        JSONObject jsons = new JSONObject(val);
-        JSONArray data = jsons.getJSONArray("data");
-        JSONObject dataObject = data.getJSONObject(0);
-        Double formatted = dataObject.getDouble("service_fee");
+//        String urlAPI = "http://140.238.54.136/api/calculateFee";
+//        String height = "100";
+//        String length = "100";
+//        String width = "100";
+//        String weight = "100";
+//        // Tạo yêu cầu tính phí vận chuyển đến API
+//        CloseableHttpClient client = HttpClientBuilder.create().build();
+//        HttpPost post = new HttpPost("http://140.238.54.136/api/calculateFee");
+//        post.setHeader("Content-type", "application/json");
+//        post.setHeader("Authorization", "Bearer " + token);
+//        JSONObject json = new JSONObject();
+//        json.put("from_district_id", from_district_id);
+//        json.put("from_ward_id", from_ward_id);
+//        json.put("to_district_id", to_district_id);
+//        json.put("to_ward_id", to_ward_id);
+//        json.put("height", height);
+//        json.put("length", length);
+//        json.put("width", width);
+//        json.put("weight", weight);
+//        StringEntity entity = new StringEntity(json.toString());
+//        post.setEntity(entity);
+//
+//        // Gửi yêu cầu và nhận phản hồi từ API
+//        HttpResponse apiResponse = client.execute(post);
+//        HttpEntity apiResponseEntity = apiResponse.getEntity();
+//        String apiResponseString = EntityUtils.toString(apiResponseEntity);
+//        // Trích xuất giá trị phí vận chuyển từ phản hồi API
+//        String val = apiResponseString;
+//        JSONObject jsons = new JSONObject(val);
+//        JSONArray data = jsons.getJSONArray("data");
+//        JSONObject dataObject = data.getJSONObject(0);
+//        Double formatted = dataObject.getDouble("service_fee");
         // Trích xuất giá trị phí vận chuyển từ phản hồi API
         // double serviceFee = Double.parseDouble(String.valueOf(apiResponseJson.getJSONArray("data").getJSONObject(0).getDouble("service_fee")));
-        return formatted;
+        return 0.0;
     }
 
     public static void main(String[] args) throws IOException {
